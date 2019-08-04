2a0f037a06e4810756395dd6c1005fdfb42367be
Rebecca D <rydagostino@gmail.com>
2019-08-03 21:00:50 -0400

    Added searching function
    Need to get the search to return the positions, got the function to step through the map.

--- mysubmission/mysolver.py
+++ mysubmission/mysolver.py
@@ -1,6 +1,27 @@
 import requests
 import numpy as np
 
+#fix why it cant find the goal and start cords..
+start = 0
+startx = 0
+starty = 0
+goalx = 0
+goaly = 0
+
+
+def explore(value):
+    if value == "X":
+        print("A wall!")
+    elif value == "A":
+        print("Found the start!")
+        startx, starty = np.where(map == "A")
+        start = zip(*np.where(map == "A"))
+    elif value == "B":
+        print("Found the goal!")
+        goalx, goaly = np.where(map == "B")
+    else:
+        print("Nothing here...")
+
 #get map to solve
 r = requests.get('https://api.noopschallenge.com/mazebot/random').json()
 #print(r.text)
@@ -14,5 +35,16 @@ maptext = r['map']
 
 map = np.asarray(maptext)
 print(map)
+print(map.shape)
+
+for n in map:
+    for j in n:
+        #print("|")
+        #print(j)
+        explore(j)
+
+print(startx, starty)
+print(goalx, goaly)
+print(start)
 
 #use a* and solve it
